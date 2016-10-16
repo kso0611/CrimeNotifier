d7ae55e2628d78478ef17fbbe77cdc9ff619ca1c
kso0611 <kso0611@gmail.com>
2016-10-16 16:14:57 +0900

    initial commit

---
+++ app/src/main/java/com/embedded/dgu/crimenotifier/IntroActivity.java
@@ -0,0 +1,38 @@
+package com.embedded.dgu.crimenotifier;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroActivity extends AppCompatActivity {
+
+    private Handler h;
+    private int delayTime = 1200;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        h = new Handler();
+        h.postDelayed(intro, delayTime);
+
+    }
+
+    Runnable intro = new Runnable() {
+        public void run() {
+            Intent i = new Intent(IntroActivity.this, MainActivity.class);
+            startActivity(i);
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
+
+        }
+    };
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        h.removeCallbacks(intro);
+    }
+}
